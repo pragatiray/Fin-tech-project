d68c42675fa272fd0076b4f43cf80bcce91e6102
pragatiray <pragatiray445@gmail.com>
2026-02-23 14:12:42 +0545

    Removing unecessary lines of code

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,7 +12,6 @@ userSchema.pre("save", async function () {
 
     const bcrypt = require("bcryptjs");
     this.password = await bcrypt.hash(this.password, 10);
-    console.log("Reseting the password");
 });
 
 module.exports = mongoose.model("User", userSchema);
