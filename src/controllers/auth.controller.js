8ae57a6a71a58fcd4c471dbf6dbe49eda63ab34c
pragatiray <pragatiray445@gmail.com>
2026-03-01 20:46:46 +0545

    Implemented register and login route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -17,14 +17,11 @@ async function registerUser(req, res) {
             });
         }
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
         // Create user
         const user = await userModel.create({
             name,
             email,
-            password: hashedPassword
+            password
         });
 
         // Create token
@@ -57,6 +54,55 @@ async function registerUser(req, res) {
     }
 }
 
+async function userLoginController(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        // Find user by email
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Email or password is invalid"
+            });
+        }
+
+        // Compare password
+        const isValidPassword = await user.comparePassword(password);
+
+        if (!isValidPassword) {
+            return res.status(401).json({
+                message: "Email or password is invalid"
+            });
+        }
+
+        // Create token
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        // Send cookie
+        res.cookie("token", token);
+
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
 module.exports = {
-    registerUser
+    registerUser,
+    userLoginController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controllers/auth.controller");
+const { registerUser,userLoginController } = require("../controllers/auth.controller");
 
 router.post("/register", registerUser);
+
+router.post("/login", userLoginController);
 
 module.exports = router;
